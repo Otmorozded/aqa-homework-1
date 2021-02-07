bfe1b093dcb73d6db53a300485036ce5a5d9573a
Evgeny Vasilev <jony.torrent@gmail.com>
2021-02-07 14:31:37 +0300

    build

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
@@ -48,5 +48,5 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
 
     }
-    
+
 }
