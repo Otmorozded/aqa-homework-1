f1c765d0093f6a6b63c1260483be43c326688083
Evgeny Vasilev <jony.torrent@gmail.com>
2021-02-07 16:03:24 +0300

    test fix

--- src/test/java/ru/netology/service/CashBackHackServiceJupiterTest.java
+++ src/test/java/ru/netology/service/CashBackHackServiceJupiterTest.java
@@ -10,11 +10,8 @@ public class CashBackHackServiceJupiterTest {
     public void shouldSumLessThan1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 100;
-
         int expected = 900;
-        service.remain(amount);
         int actual = service.remain(amount);
-
         assertEquals(actual, expected);
     }
 
@@ -24,14 +21,9 @@ public class CashBackHackServiceJupiterTest {
 
         CashbackHackService service = new CashbackHackService();
         int amount = 1500;
-
         int expected = 500;
-        service.remain(amount);
         int actual = service.remain(amount);
-
         assertEquals(actual, expected);
-
-
     }
 
     @Test
@@ -40,16 +32,9 @@ public class CashBackHackServiceJupiterTest {
 
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
-
         int expected = 0;
-        service.remain(amount);
         int actual = service.remain(amount);
-
         assertEquals(actual, expected);
-
     }
-
-
-
 
 }
--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -10,43 +10,30 @@ public class CashbackHackServiceTest {
     public void shouldSumLessThan1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 100;
-
         int expected = 900;
-        service.remain(amount);
         int actual = service.remain(amount);
-
         assertEquals(actual, expected);
 
     }
 
     @Test
     public void shouldSumOver1000() {
-
         CashbackHackService service = new CashbackHackService();
         int amount = 1500;
-
         int expected = 500;
-        service.remain(amount);
         int actual = service.remain(amount);
-
         assertEquals(actual, expected);
-
 
     }
 
     @Test
 
     public void shouldSumEqual1000() {
-
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
-
         int expected = 0;
-        service.remain(amount);
         int actual = service.remain(amount);
-
         assertEquals(actual, expected);
-
     }
 
 }
